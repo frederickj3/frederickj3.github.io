c4cb5a9d5abc6e90aecf2e5987bd42721d8094aa
frederickj3 <1joshfrederick@gmail.com>
2023-01-17 19:17:57 -0500

    Finished ICE1

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,10 @@
 "use strict";
+/**
+ * Name: Josh Frederick
+ * Date: January, 10 2023
+ * File Name: app.js
+ * File Description: JavaScript file that holds functions
+ */
 //IIFE - Immediately Invoked Function Expressions
 //AKA - Anonymous Self-Executing Function
 
@@ -10,6 +16,31 @@
             location.href = "about.html";
         });
     }
+    // ICE1
+    // Steps 3 and 4 - Creating variables equal to the first tag name "main"'s value and a paragraph tag
+    let MainContent = document.getElementsByTagName("main")[0];
+    let MainParagraph = document.createElement("p")
+    // Setting up for step 8
+    let DocumentBody = document.body;
+
+    // Step 5 - Assign an id and class to the main paragraph variable created above along with text for it
+    MainParagraph.setAttribute("id", "MainParagraph")
+    MainParagraph.setAttribute("class", "mt-3")
+    MainParagraph.textContent = "This is the Main Paragraph!"
+    // Step 6 - Append the above variable to the MainContent variable created above
+    MainContent.appendChild(MainParagraph);
+    // Step 7 - Using template strings to concatenate two strings and display them as one line
+    let FirstString = "This is";
+    let SecondString = `${FirstString} the Main Paragraph.`;
+    MainParagraph.textContent = SecondString;
+
+    // Step 8 - Using template strings to create an HTML article  and show it on the document body
+    let Article = document.createElement("article");
+    let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3"> This is my article paragraph</p>`;
+    Article.setAttribute("class", "container")
+    Article.innerHTML = ArticleParagraph;
+    DocumentBody.appendChild(Article);
+
     function DisplayProductPage(){
 
     }
@@ -22,6 +53,7 @@
     function DisplayContactPage(){
 
     }
+    // Start function to call the methods listed above in a switch case on start up
     function Start(){
         console.log("App Started!")
         switch(document.title){
